c72b11e43896391bb932b9b36399d28ebdb32890
abarrera <abarrera@biociphers.org>
2014-08-20 16:23:26 -0400

    Added support for chromosome to Voila

--- voila/splice_graphics/geneGraphic.py
+++ voila/splice_graphics/geneGraphic.py
@@ -5,11 +5,12 @@ __author__ = 'abarrera'
 
 class GeneGraphic(object):
 
-    def __init__(self, name, strand, exons, junctions):
+    def __init__(self, name, strand, exons, junctions, chrom=None):
         self.name = name
         self.strand = strand
         self.exons = exons
         self.junctions = junctions
+        self.chrom = chrom
 
     def get_name(self):
         return self.name
@@ -22,6 +23,15 @@ class GeneGraphic(object):
 
     def get_junctions(self):
         return self.junctions
+
+    def get_chrom(self):
+        return self.chrom
+
+    def get_coords(self):
+        if self.strand == '+':
+            return [self.exons[0].coords[0], self.exons[-1].coords[1]]
+        if self.strand == '-':
+            return [self.exons[-1].coords[1], self.exons[0].coords[0]]
 
     def to_JSON(self, encoder=json.JSONEncoder):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, cls=encoder)
