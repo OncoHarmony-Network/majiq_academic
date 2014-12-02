9b964f7a599e72266f63aa8de1a6c89c571dfed7
jordi <jordi@biociphers.org>
2014-12-02 10:33:34 -0500

    Remove  jump into targets/ source restriction and add containing on duplications checks.

--- grimoire/lsv.py
+++ grimoire/lsv.py
@@ -272,7 +272,8 @@ class LSV(object):
         if self.type == variant.type:
             return False
         if np.array_equal(jlist1, jlist2):
-            if (self.exon.get_strand() == '+' and self.lsv_type == SSOURCE) or (self.exon.get_strand() == '-' and self.lsv_type == STARGET):
+            if (self.exon.get_strand() == '+' and self.type == SSOURCE) or \
+                    (self.exon.get_strand() == '-' and self.type == STARGET):
                 res = False
             else:
                 res = True
