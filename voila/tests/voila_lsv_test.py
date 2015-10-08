ef39a4724deb74042a2536f8131dedb9d28209d4
abarrera <abarrera@biociphers.org>
2015-10-08 14:07:25 -0400

    Removed unused files and added first test files in VOILA

--- voila/tests/voila_lsv_test.py
+++ voila/tests/voila_lsv_test.py
@@ -3,6 +3,7 @@ from nose.tools import assert_equal
 from nose.tools import assert_not_equal
 from nose.tools import assert_raises
 from nose.tools import raises
+from nose import with_setup
 import os
 try:
     import cPickle as pkl
@@ -11,28 +12,36 @@ except ImportError:
 from voila.vlsv import VoilaLsv
 import numpy as np
 
-LSVGRAPHIC_FILENAME = os.path.join(os.path.dirname(__file__), 'data/lsv_splicegraph.pickle')
+LSVGRAPHIC_FILENAME = os.path.join(os.path.dirname(__file__), 'data/lsv_graphic.pickle')
 
 
-class VoilaLsvTest(object):
+class TestVoilaLsv(object):
     @classmethod
     def setup_class(cls):
         """This method is run once for each class before any tests are run"""
         sg = pkl.load(open(LSVGRAPHIC_FILENAME))
         binsl = []
-        for ii in xrange(3):
+        num_juncs = 3
+        for ii in xrange(num_juncs):
             binsl.append(np.random.rand(40))
             binsl[-1] /= binsl[-1].sum()
 
-        test_instance = cls()
-        test_instance.sg = sg
-        test_instance.binsl = binsl
+        cls.psi1 = []
+        for ii in xrange(num_juncs):
+            cls.psi1.append(np.random.rand(20))
+            cls.psi1[-1] /= cls.psi1[-1].sum()
 
-        return test_instance
+        cls.psi2 = []
+        for ii in xrange(num_juncs):
+            cls.psi2.append(np.random.rand(20))
+            cls.psi2[-1] /= cls.psi2[-1].sum()
 
-    @classmethod
-    def teardown_class(cls):
-        """This method is run once for each class _after_ all tests are run"""
+        cls.sg = sg
+        cls.binsl = binsl
+
+    # @classmethod
+    # def teardown_class(cls):
+    #     """This method is run once for each class _after_ all tests are run"""
 
     def setUp(self):
         """This method is run once before _each_ test method is executed"""
@@ -40,13 +49,18 @@ class VoilaLsvTest(object):
     def teardown(self):
         """This method is run once after _each_ test method is executed"""
 
-    def initTest(self):
-        a = VoilaLsv(self.binsl, self.sg)
-        assert_equal(a.psi1, None)
-        assert_not_equal(a.bins, None)
+    def test_init_psi(self):
+        a = VoilaLsv(TestVoilaLsv.binsl, self.sg)
+        assert_equal(a.is_delta_psi(), False)
+        assert_not_equal(a.is_delta_psi(), True)
+
+    def test_init_dpsi(self):
+        a = VoilaLsv(TestVoilaLsv.binsl, self.sg, psi1=TestVoilaLsv.psi1, psi2=TestVoilaLsv.psi2)
+        assert_equal(a.is_delta_psi(), True)
+        assert_not_equal(a.is_delta_psi(), False)
 
     # def test_return_true(self):
-    #     a = A()
+    #     a = VoilaLsv(TestVoilaLsv.binsl, self.sg)
     #     assert_equal(a.return_true(), True)
     #     assert_not_equal(a.return_true(), False)
     #
