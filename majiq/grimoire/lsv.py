Gc normalization fixed

majiq/grimoire/lsv.py
@@ -463,7 +463,7 @@ class Queue_Lsv(object):
                 self.coverage[i, j] *= majiq_config.gc_factor[exp_idx](dummy)
         del self.gc_factor
 
-    def to_hdf5(self, hdf5grp, lsv_idx, exp_idx, gc=None):
+    def to_hdf5(self, hdf5grp, lsv_idx, exp_idx, gc_func=None):
 
         try:
             njunc = len(self.junction_id)
@@ -471,20 +471,17 @@ class Queue_Lsv(object):
                 shp = hdf5grp[LSV_JUNCTIONS_DATASET_NAME].shape
                 shp_new = shp[0] + majiq_config.nrandom_junctions
                 hdf5grp[LSV_JUNCTIONS_DATASET_NAME].resize((shp_new, shp[1]))
-                if majiq_config.gcnorm:
-                    gc.resize((shp_new, shp[1]))
-
-
-            hdf5grp[LSV_JUNCTIONS_DATASET_NAME][lsv_idx:lsv_idx+njunc, :] = self.coverage[:, exp_idx, :]
 
             if majiq_config.gcnorm:
-                gc[lsv_idx:lsv_idx+njunc, :] = self.gc_factor
-
+                vals = gc_func(self.gc_factor)
+                self.coverage[:, exp_idx, :] = np.multiply(self.coverage[:, exp_idx, :], vals)
+            hdf5grp[LSV_JUNCTIONS_DATASET_NAME][lsv_idx:lsv_idx+njunc, :] = self.coverage[:, exp_idx, :]
 
             h_lsv = hdf5grp.create_group("LSVs/%s" % self.id)
             h_lsv.attrs['coords'] = self.coords
             h_lsv.attrs['id'] = self.id
             h_lsv.attrs['type'] = self.type
+
             h_lsv.attrs['coverage'] = hdf5grp[LSV_JUNCTIONS_DATASET_NAME].regionref[lsv_idx:lsv_idx + njunc]
             self.visual[exp_idx].to_hdf5(h_lsv)
         except:

majiq/new_builder/exon.py
@@ -38,7 +38,7 @@ class Exon:
         self.ss_3p_list = []
         self.ss_5p_list = []
         self.id = "%s:%d-%d" % (self.gene_name, start, end)
-        # sedlf.strand = strand
+        # self.strand = strand
         self.gc_content = 0
         self.coverage = np.zeros(shape=config.num_experiments)
         self.score = None

majiq/src/normalize.py
@@ -53,7 +53,21 @@ def mark_stacks(lsv_list, fitfunc_r, pvalue_limit, logger=None):
     return lsv_list
 
 
-def gc_normalization(lsv_list, gc_content_files, gc_pairs, logger):
+def gc_normalization(gc_pairs, logger):
+
+    logger.info("Gc Content normalization")
+    factor, meanbins = gc_factor_calculation(gc_pairs, nbins=10)
+    v_gcfactor_func = [None] * majiq_config.num_experiments
+
+    for exp_n in xrange(majiq_config.num_experiments):
+        a = np.append(factor[exp_n], factor[exp_n][-1])
+        gc_factor = interpolate.interp1d(meanbins[exp_n], factor[exp_n], bounds_error=False, fill_value=1)
+        v_gcfactor_func[exp_n] = np.vectorize(gc_factor)
+
+    return v_gcfactor_func
+
+
+def gc_normalization_old(lsv_list, gc_content_files, gc_pairs, logger):
 
     logger.info("Gc Content normalization")
     factor, meanbins = gc_factor_calculation(gc_pairs, nbins=10)
@@ -129,8 +143,6 @@ def gc_factor_calculation(gc_pairs, nbins=10):
             local_factor[exp_n] = gc_factor
 
     return local_factor, local_meanbins
-
-
 
 
 def prepare_gc_content(gn):