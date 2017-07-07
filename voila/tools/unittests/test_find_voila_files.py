3462bf638e638b5c7022c6b82e3c35eb5e72bbaa
cradens <cradens@biociphers.org>
2017-07-07 08:46:41 -0400

    find voila files unit test can I use ~ to specify home? ANS=NO Instead do this

--- voila/tools/unittests/test_find_voila_files.py
+++ voila/tools/unittests/test_find_voila_files.py
@@ -6,4 +6,6 @@ from voila.tools.find_voila_files import get_voila_files
 class TestStringMethods(unittest.TestCase):
 
     def test_hello_world(self):
-        self.assertEqual(get_voila_files(directory="~/", pattern="doesntexist"), [])
+        from os.path import expanduser
+        home = expanduser("~")
+        self.assertEqual(get_voila_files(directory=home, pattern="doesntexist"), [])
