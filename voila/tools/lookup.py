5edec947d4e8551fd4d3fe2e2015bf830d2a0cba
cradens <cradens@biociphers.org>
2017-08-29 09:32:40 -0400

    lookup.py handles stop_at to be a file

--- voila/tools/lookup.py
+++ voila/tools/lookup.py
@@ -113,7 +113,7 @@ class ThisisLookup(Tool):
             abbreviated_bool = False
         for lookup_val in the_lookup_vals:
             details = "dPSI cutoff: %s\nProb cutoff: %s\n" % (args.dpsi_thresh, args.prob_dpsi_thresh)
-            print("=-=-=-=-=\n=-=-=-=-=\n=-=-=-=-=\n %s\n%s =-=-=-=-=\n=-=-=-=-=\n=-=-=-=-=\n" % lookup_val, details)
+            print("=-=-=-=-=\n=-=-=-=-=\n=-=-=-=-=\n %s\n%s =-=-=-=-=\n=-=-=-=-=\n=-=-=-=-=\n" % (lookup_val, details))
 
             lookup_everywhere(dictionary_lookup=imported,
                               name=lookup_val,
