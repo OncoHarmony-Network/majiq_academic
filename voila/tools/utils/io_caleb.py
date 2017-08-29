8305aa7fc0ebf9a3e8c1f2a90b5ed0ade3aab2e8
cradens <cradens@biociphers.org>
2017-08-29 11:18:28 -0400

    lookup.py debug dpsi thresh

--- voila/tools/utils/io_caleb.py
+++ voila/tools/utils/io_caleb.py
@@ -286,7 +286,7 @@ def import_voila_txt(fp,
                         break
             elif isinstance(stop_at, list):
                 if fp == "/data/THelper/lisp/voila/dpsi/groups/thresh_10/TU_T0_72vsTU_T17_72/TU_T0_72_TU_T17_72.deltapsi_deltapsi.tsv":
-                    if gene[0] in line:
+                    if stop_at[0] in line:
                         pdb.set_trace()
                 gene_in_line_bools = [gene in line for gene in stop_at]
                 if True in gene_in_line_bools:
