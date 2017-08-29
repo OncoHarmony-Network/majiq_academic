094f7711731b4a9fbcd56dfd2bf165ecbf7afe18
jordi <jordi@biociphers.org>
2017-08-29 15:58:16 -0400

    fix intron retention crash for nchunks not properly set

--- majiq/src/build.py
+++ majiq/src/build.py
@@ -249,7 +249,7 @@ class Builder(BasicPipeline):
             p.join()
 
             if self.nthreads > 1:
-                pool = mp.Pool(processes=self.nthreads, initializer=process_conf, initargs=[self, None, None, None])
+                pool = mp.Pool(processes=self.nthreads, initializer=process_conf, initargs=[self, None])
                 lchnksize = max(int(len(majiq_config.sam_list)/self.nthreads), 1) + 1
                 pool.map_async(parsing_files, majiq_utils.chunks2(majiq_config.sam_list, lchnksize,
                                                                   extra=range(self.nthreads)))
