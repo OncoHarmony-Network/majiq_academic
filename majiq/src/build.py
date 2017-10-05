4db60cb5fd3367050f8bcac2b3c03cf42c702119
jordi <jordi@biociphers.org>
2017-10-05 11:30:06 -0400

    Fix het pipeline

--- majiq/src/build.py
+++ majiq/src/build.py
@@ -80,10 +80,13 @@ def parse_denovo_elements(pipe_self, logger):
     dict_junctions = {}
     list_introns = {}
 
-    nthreads = min(pipe_self.nthreads, len(majiq_config.sam_list))
+    # nthreads = min(pipe_self.nthreads, len(majiq_config.sam_list))
+
+    lchnksize = math.ceil(len(majiq_config.sam_list) / pipe_self.nthreads)
+    nthreads = min(pipe_self.nthreads, math.ceil(len(majiq_config.sam_list) / lchnksize))
     logger.info("Create %s processes" % nthreads)
     pipe_self.lock = [mp.Lock() for _ in range(nthreads)]
-    lchnksize = max(math.ceil(len(majiq_config.sam_list) / nthreads), 1)
+    print (nthreads, len(majiq_config.sam_list), lchnksize)
     pool1 = mp.Pool(processes=nthreads, initializer=majiq_multi.process_conf, initargs=[find_new_junctions, pipe_self],
                     maxtasksperchild=1)
     pool2 = mp.Pool(processes=nthreads, initializer=majiq_multi.process_conf, initargs=[find_new_introns, pipe_self],
