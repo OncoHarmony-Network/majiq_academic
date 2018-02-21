0d0acaf5984dd0c13af016900bc4ed3b805d5e71
Christopher Green <grech@upenn.edu>
2018-02-21 15:46:08 -0500

    Added import to majiq's lsv file.

--- majiq/grimoire/lsv.pyx
+++ majiq/grimoire/lsv.pyx
@@ -3,7 +3,7 @@ from majiq.src.sample import sample_from_junctions
 from majiq.grimoire.exon cimport Exon
 from majiq.grimoire.junction cimport Junction
 from majiq.src.normalize import mark_stacks
-
+cimport numpy as np
 from voila.constants import *
 from voila.splice_graphics import ExonGraphic, LsvGraphic, JunctionGraphic
 import collections
