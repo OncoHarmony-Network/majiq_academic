236224c53e5b05ac187f6d938dbe9436d751570b
jordi <jordi@biociphers.org>
2018-06-13 12:46:45 -0400

    debug for ir duplicate

--- majiq/src/internals/seq_parse.pyx
+++ majiq/src/internals/seq_parse.pyx
@@ -88,13 +88,20 @@ cdef int _output_lsv_file_single(vector[LSV*] out_lsvlist, str experiment_name, 
                     key = junc_ids[i][0].split(b':')[0]
                     irv = find_intron_retention(gene_map[key], junc_ids[i][1], junc_ids[i][2])
 
-                    kk = [ print(ir_ptr.get_gene(), ir_ptr.get_start(), ir_ptr.get_end()) for ir_ptr in irv ]
-
-                    print("######\n")
+                    # for ir_ptr in irv :
+                    #     print(ir_ptr.get_gene(), ir_ptr.get_start(), ir_ptr.get_end())
+                    #
+                    # print("######\n")
                     for ir_ptr in irv:
 
-                        sg.intron_retention(ir_ptr.get_gene().get_id().decode('utf-8'), ir_ptr.get_start(),
-                                            ir_ptr.get_end()).update_reads(experiment_name, junc_ids[i][3])
+                        xx = sg.intron_retention(ir_ptr.get_gene().get_id().decode('utf-8'), ir_ptr.get_start(),
+                                                 ir_ptr.get_end()).update_reads(experiment_name, junc_ids[i][3])
+                        if xx == -1:
+                            print ("ERRORRRRR", ir_ptr.get_gene().get_id().decode('utf-8'), ir_ptr.get_start(),
+                                                 ir_ptr.get_end())
+
+
+
                         tlb_ir[ir_ptr.get_key(ir_ptr.get_gene())] = Jinfo(i, junc_ids[i][1], junc_ids[i][2],
                                                                   junc_ids[i][3], junc_ids[i][4])
 
