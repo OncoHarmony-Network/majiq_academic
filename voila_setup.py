7315ea75b5da63b038dde3dc008d9c58d0ebe411
Christopher Green <grech@upenn.edu>
2018-10-29 16:53:16 -0400

    Small update to voila install script.

--- voila_setup.py
+++ voila_setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
+from voila.constants import VERSION
 
 setup(
     name='voila',
+    version=VERSION,
     packages=['voila', 'voila.api', 'voila.flask_proj', 'voila.utils', 'voila.view'],
     install_requires=['h5py', 'flask', 'sqlalchemy', 'waitress', 'scipy'],
     entry_points={'console_scripts': ['voila = voila.run_voila:main']},
