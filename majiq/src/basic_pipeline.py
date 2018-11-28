fix pseudo count to 1e-30 for prior calculation

majiq/src/basic_pipeline.py
@@ -14,14 +14,13 @@ import multiprocessing as mp
 ################################
 
 
-def bootstrap_samples_with_divs(list_of_lsv, chnk, conf, logger):
-
-    lsvs_to_work = majiq_io.get_extract_lsv_list(list_of_lsv, conf.file_list, aggr=False)
-    divs = divs_from_bootsamples(lsvs_to_work, n_replica=len(conf.file_list), pnorm=1, nbins=conf.nbins,
-                                 lsv_types=conf.lsv_type_dict)
-    qm = QueueMessage(QUEUE_MESSAGE_BOOTSTRAP, ([xx.id for xx in lsvs_to_work], divs), chnk)
-    conf.queue.put(qm, block=True)
-
+# def bootstrap_samples_with_divs(list_of_lsv, chnk, conf, logger):
+#
+#     lsvs_to_work = majiq_io.get_extract_lsv_list(list_of_lsv, conf.file_list, aggr=False)
+#     divs = divs_from_bootsamples(lsvs_to_work, n_replica=len(conf.file_list), pnorm=1, nbins=conf.nbins,
+#                                  lsv_types=conf.lsv_type_dict)
+#     qm = QueueMessage(QUEUE_MESSAGE_BOOTSTRAP, ([xx.id for xx in lsvs_to_work], divs), chnk)
+#     conf.queue.put(qm, block=True)
 
 def pipeline_run(pipeline):
     """ Exception catching for all the pipelines """
@@ -33,9 +32,6 @@ def pipeline_run(pipeline):
 
 
 class BasicPipeline:
-    #
-    # boots_conf = collections.namedtuple('boots_conf', 'm, k, discardzeros, trimborder')
-    # basic_conf = collections.namedtuple('basic_conf', 'output, nbins, silent, debug')
 
     def __init__(self, args):
         """Basic configuration shared by all pipelines"""
@@ -66,46 +62,46 @@ class BasicPipeline:
     #             self.logger.debug("Fitting NB function with constitutive events...")
     #         return fit_nb(const_junctions, "%s/nbfit" % self.outDir, self.plotpath, logger=self.logger)
 
-    def calc_weights(self, weight_type, list_of_lsv, name, file_list, logger=None):
-        self.file_list = file_list
-        if weight_type.lower() == WEIGTHS_AUTO and len(self.file_list) >= 3:
-            """ Calculate bootstraps samples and weights """
-            nthreads = min(self.nthreads, len(list_of_lsv))
-
-            pool = mp.Pool(processes=nthreads, initializer=process_conf,
-                           initargs=[bootstrap_samples_with_divs, self],
-                           maxtasksperchild=1)
-
-            [xx.acquire() for xx in self.lock]
-            pool.map_async(process_wrapper, chunks(list_of_lsv, nthreads))
-            pool.close()
-            divs = []
-            lsvs = []
-            queue_manager(output_h5dfp=None, lock_array=self.lock, result_queue=self.queue,
-                          num_chunks=nthreads, out_inplace=(lsvs, divs), logger=logger)
-            pool.join()
-            # lsvs = {xx: vv for xx, vv in enumerate(lsvs)}
-            divs = np.array(divs)
-            rho = calc_rho_from_divs(divs, thresh=self.weights_threshold, alpha=self.weights_alpha,
-                                     nreps=len(self.file_list), logger=self.logger)
-
-            wgts = calc_local_weights(divs, rho, self.local)
-            if logger is not None:
-                logger.info('Global weights for %s are: %s' % (name, ','.join([str(x) for x in rho])))
-
-            majiq_io.store_weights(lsvs, wgts, self.outDir, name)
-            wgts = None
-
-        elif weight_type.lower() == WEIGTHS_NONE or len(self.file_list) < 3:
-            wgts = np.ones(shape=(len(self.file_list)))
-
-        else:
-            wgts = np.array([float(xx) for xx in weight_type.split(',')])
-            if len(wgts) != len(self.file_list):
-                logger.error('weights wrong arguments number of weights values is different than number of '
-                             'specified replicas for that group')
-        del self.file_list
-        return wgts
+    # def calc_weights(self, weight_type, list_of_lsv, name, file_list, logger=None):
+    #     self.file_list = file_list
+    #     if weight_type.lower() == WEIGTHS_AUTO and len(self.file_list) >= 3:
+    #         """ Calculate bootstraps samples and weights """
+    #         nthreads = min(self.nthreads, len(list_of_lsv))
+    #
+    #         pool = mp.Pool(processes=nthreads, initializer=process_conf,
+    #                        initargs=[bootstrap_samples_with_divs, self],
+    #                        maxtasksperchild=1)
+    #
+    #         [xx.acquire() for xx in self.lock]
+    #         pool.map_async(process_wrapper, chunks(list_of_lsv, nthreads))
+    #         pool.close()
+    #         divs = []
+    #         lsvs = []
+    #         queue_manager(output_h5dfp=None, lock_array=self.lock, result_queue=self.queue,
+    #                       num_chunks=nthreads, out_inplace=(lsvs, divs), logger=logger)
+    #         pool.join()
+    #         # lsvs = {xx: vv for xx, vv in enumerate(lsvs)}
+    #         divs = np.array(divs)
+    #         rho = calc_rho_from_divs(divs, thresh=self.weights_threshold, alpha=self.weights_alpha,
+    #                                  nreps=len(self.file_list), logger=self.logger)
+    #
+    #         wgts = calc_local_weights(divs, rho, self.local)
+    #         if logger is not None:
+    #             logger.info('Global weights for %s are: %s' % (name, ','.join([str(x) for x in rho])))
+    #
+    #         majiq_io.store_weights(lsvs, wgts, self.outDir, name)
+    #         wgts = None
+    #
+    #     elif weight_type.lower() == WEIGTHS_NONE or len(self.file_list) < 3:
+    #         wgts = np.ones(shape=(len(self.file_list)))
+    #
+    #     else:
+    #         wgts = np.array([float(xx) for xx in weight_type.split(',')])
+    #         if len(wgts) != len(self.file_list):
+    #             logger.error('weights wrong arguments number of weights values is different than number of '
+    #                          'specified replicas for that group')
+    #     del self.file_list
+    #     return wgts
 
 
 