27d21d8d257c2e212e3e09f27d5bf64afdbe4a49
Christopher Green <grech@upenn.edu>
2018-12-12 10:28:44 -0500

    Another attempt to fix the voila install script.

--- voila_setup.py
+++ voila_setup.py
@@ -13,7 +13,7 @@ setup(
     name='voila',
     version=VERSION,
     packages=['voila', 'voila.api', 'voila.view', 'voila.utils', 'voila.view'],
-    install_requires=['h5py', 'scipy', 'flask', 'flask_wtf', 'waitress'],
+    install_requires=['Flask-WTF', 'Flask', 'h5py', 'scipy', 'waitress'],
     entry_points={'console_scripts': ['voila = voila.run_voila:main']},
     package_data={
         'voila': [
