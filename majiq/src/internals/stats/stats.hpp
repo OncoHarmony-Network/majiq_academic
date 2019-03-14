07d448b39584417781794a5ab6030ab670c3d2ea
jordi <jordi@biociphers.org>
2019-03-14 11:01:22 -0400

    Fix wilcoxon problem task.... real problem is order of statistics names.

--- majiq/src/internals/stats/stats.hpp
+++ majiq/src/internals/stats/stats.hpp
@@ -19,14 +19,13 @@ class HetStats{
         std::vector<MajiqStats::TestStat *> statistics ;
 
         bool initialize_statistics(std::vector<string> list_stats){
-            sort(list_stats.begin(), list_stats.end())
+            sort(list_stats.begin(), list_stats.end()) ;
             std::vector<string>::iterator it;
             it = std::unique (list_stats.begin(), list_stats.end());
             list_stats.resize( std::distance(list_stats.begin(),it) );
 
             for (it=list_stats.begin(); it!=list_stats.end(); ++it){
                 string st = *it ;
-cerr << " KKLL " << st << "\n" ;
                 if (st == "TNOM")
                     statistics.push_back((MajiqStats::TestStat*) new MajiqStats::TNOM()) ;
                 if (st == "WILCOXON")
@@ -37,14 +36,14 @@ cerr << " KKLL " << st << "\n" ;
                     statistics.push_back((MajiqStats::TestStat*) new MajiqStats::tTest()) ;
                 if (st == "ALL"){
                     statistics.clear() ;
-                    statistics.push_back((MajiqStats::TestStat*) new MajiqStats::TNOM()) ;
-                    names.push_back("TNOM") ;
-                    statistics.push_back((MajiqStats::TestStat*) new MajiqStats::Wilcoxon()) ;
-                    names.push_back("WILCOXON") ;
                     statistics.push_back((MajiqStats::TestStat*) new MajiqStats::InfoScore()) ;
                     names.push_back("INFOSCORE") ;
+                    statistics.push_back((MajiqStats::TestStat*) new MajiqStats::TNOM()) ;
+                    names.push_back("TNOM") ;
                     statistics.push_back((MajiqStats::TestStat*) new MajiqStats::tTest()) ;
                     names.push_back("TTEST") ;
+                    statistics.push_back((MajiqStats::TestStat*) new MajiqStats::Wilcoxon()) ;
+                    names.push_back("WILCOXON") ;
                     break ;
                 } else
                     names.push_back(st) ;
