f429dbbb27b7348110f98605282476ee84e1324c
jordi <jordi@biociphers.org>
2019-03-14 17:54:33 -0400

    fix number of experiment calculations that always returned 0 for min_exp < 1

--- majiq/run_majiq.py
+++ majiq/run_majiq.py
@@ -30,6 +30,13 @@ def get_minsamps(param):
     assert param >= 2
     return param
 
+def check_positive(value):
+    ivalue = float(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive float value" % value)
+    return ivalue
+
+
 
 def new_subparser():
     return argparse.ArgumentParser(add_help=False)
@@ -59,7 +66,7 @@ def main():
     common.add_argument('--mem-profile', default=False, action='store_true',
                         help="Print memory usage summary at the end of the execution. [Default: %(default)s]")
 
-    common.add_argument('--min-experiments', default=0.5, type=float, dest='min_exp',
+    common.add_argument('--min-experiments', default=0.5, type=check_positive, dest='min_exp',
                         help='Lower threshold for group filters. min_experiments set the minimum number of experiments '
                              'where the different filters check in order to pass an lsv or junction.\n'
                              '\t + <  1 the value is the fraction of the experiments in the group\n'
