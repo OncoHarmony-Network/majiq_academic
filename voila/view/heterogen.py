962853361cefa59bf503808c94eaabac0a292035
pjewell <pjewell@biociphers.org>
2019-04-17 13:18:38 -0400

    -fixed pairwise data plot, read from wrong heatmaps before, now reads directly from elements

--- voila/view/heterogen.py
+++ voila/view/heterogen.py
@@ -305,7 +305,6 @@ def summary_table():
                     'junction_idx': junc_idx,
                     'mean_psi': mean_psi,
                     'mu_psi': mu_psi,
-                    'heatmap': heatmap,
                 },
                 {
                     'heatmap': heatmap,
