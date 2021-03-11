120acda68599fbc904a770149eebc957e4cef61c
Joseph K Aicher <jaicher@biociphers.org>
2021-03-11 03:39:03 -0500

    Start scripts

---
+++ src/new_majiq/_run/_run.py
@@ -0,0 +1,72 @@
+"""
+_run.py
+
+Shared types, functions for majiq-tools subcommands
+
+Author: Joseph K Aicher
+"""
+
+import argparse
+import sys
+from typing import Callable
+from new_majiq.logger import setup_logger, get_logger
+
+
+class GenericSubcommand(object):
+    """ Wrap add_args, run with shared setup (especially for logging)
+    """
+
+    def __init__(
+        self,
+        DESCRIPTION: str,
+        add_args: Callable[[argparse.ArgumentParser], None],
+        run: Callable[[argparse.Namespace], None],
+    ) -> None:
+        self._description = DESCRIPTION
+        self._add_args = add_args
+        self._run = run
+        return
+
+    @property
+    def DESCRIPTION(self) -> str:
+        return self._description
+
+    def add_args(self, parser: argparse.ArgumentParser) -> None:
+        """ Add arguments to provided argument parser
+        """
+        self._add_args(parser)
+        # add parameters for logging
+        parser.add_argument(
+            "--logger",
+            type=str,
+            default=None,
+            help="Redirect logging to specified file (default: stderr)",
+        )
+        parser.add_argument(
+            "--silent", action="store_true", default=False, help="Silence the logger"
+        )
+        parser.add_argument(
+            "--debug",
+            action="store_true",
+            default=False,
+            help="Enable detailed logging for debugging purposes",
+        )
+        return
+
+    def run(self, args: argparse.Namespace) -> None:
+        """ Run subcommand with parsed arguments
+        """
+        # set up logging
+        setup_logger(logfile=args.logger, silent=args.silent, debug=args.debug)
+        log = get_logger()
+        # run subcommand
+        try:
+            self._run(args)
+        except Exception:
+            # log exception and re-raise
+            log.exception("Exiting due to exception:")
+            sys.exit(-1)
+            return
+        # when done running, note that it was successsfully completed!
+        log.info("Finished successfully!")
+        return
---
+++ src/new_majiq/_run/tools.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+tools.py
+
+Provides entry-point to different majiq-tools scripts
+
+Author: Joseph K Aicher
+"""
+
+import argparse
+import sys
+from typing import Optional, List, Dict
+from new_majiq._run._run import GenericSubcommand
+from new_majiq._run.cite import subcommand as cite
+# from majiq_tools._run.constitutive import subcommand as constitutive
+# from majiq_tools._run.junction import subcommand as junction
+# from majiq_tools._run.sg_hdf import subcommand as sg_hdf
+# from majiq_tools._run.sg_sqlite import subcommand as sg_sqlite
+# from majiq_tools._run.sg_combine import subcommand as sg_combine
+# from majiq_tools._run.sjsg2majiq import subcommand as sjsg2majiq
+# from majiq_tools._run.sj_frequencies import subcommand as sj_frequencies
+# from majiq_tools._run.psi_frequencies import subcommand as psi_frequencies
+# from majiq_tools._run.quantify import subcommand as quantify
+# from majiq_tools._run.compare_frequencies import subcommand as compare_frequencies
+
+
+SUBPARSER_SOURCES: Dict[str, GenericSubcommand] = {
+    "cite": cite,
+}
+
+
+def main(sys_args: Optional[List[str]] = None) -> None:
+    """ Entry-point into multiple tools using subcommands
+    """
+    # build parser
+    parser = argparse.ArgumentParser(
+        description="Tools to detect, quantify, and analyze RNA splicing"
+    )
+    # add subparsers
+    subparsers = parser.add_subparsers(required=True, help="")
+    for src_name, src_module in SUBPARSER_SOURCES.items():
+        src_parser = subparsers.add_parser(
+            src_name, help=src_module.DESCRIPTION, description=src_module.DESCRIPTION,
+        )
+        src_parser.set_defaults(func=src_module.run)
+        src_module.add_args(src_parser)
+
+    # check length of input
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    # parse arguments now
+    args = parser.parse_args(sys_args)
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
