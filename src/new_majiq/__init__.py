e75852ea7dfff8b408c05b11d6b98939a06c92ad
Joseph K Aicher <jaicher@biociphers.org>
2021-03-25 14:00:28 -0400

    Expose exons, introns, junctions at top level

--- src/new_majiq/__init__.py
+++ src/new_majiq/__init__.py
@@ -11,6 +11,9 @@ import new_majiq.constants as constants
 
 # model of genes, exons connected by introns/junctions
 from .SpliceGraph import SpliceGraph
+from .GeneIntrons import GeneIntrons
+from .GeneJunctions import GeneJunctions
+from .Exons import Exons
 
 # how we measure junctions and introns from BAM files
 from .SJIntronsBins import SJIntronsBins
