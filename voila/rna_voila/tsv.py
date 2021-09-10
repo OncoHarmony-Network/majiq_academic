18b8d19b453d33439788bd0d504e2e4e6a2cc8f2
Joseph K Aicher <jaicher@biociphers.org>
2021-09-10 17:35:11 -0400

    Output dictionary of group sizes rather than list of just group names
    It is of interest to know how big quantification groups are. So, in
    metadata we output dictionary mapping group names to number of
    experiments. An alternative would be to directly print all input
    experiments, but that could be potentially overwhelming.

--- voila/rna_voila/tsv.py
+++ voila/rna_voila/tsv.py
@@ -119,10 +119,14 @@ class AnalysisTypeTsv:
 
     def get_base_metadata(self):
         with self.view_matrix() as m:
-            metadata = {'voila_version': constants.VERSION,
-                        'command': ' '.join(sys.argv),
-                        'group_names': m.group_names
-                        }
+            metadata = {
+                'voila_version': constants.VERSION,
+                'command': ' '.join(sys.argv),
+                'group_sizes': {
+                    grp: len(names)
+                    for grp, names in zip(m.group_names, m.experiment_names)
+                },
+            }
 
         return metadata
 
