28fd257f9d2482f44da9a3fc2addb3a465e59adc
Joseph K Aicher <jaicher@biociphers.org>
2021-10-12 13:45:39 -0400

    Rewrite of new-majiq quantify to implement (multi)-PSI TSV

--- src/new_majiq/_run/quantify.py
+++ src/new_majiq/_run/quantify.py
@@ -1,120 +1,146 @@
 """
 quantify.py
 
-Quantify input event coverage files
+Quantify input PsiCoverage files
 
 Author: Joseph K Aicher
 """
 
 import argparse
+import sys
 from pathlib import Path
 from typing import List, Optional
 
+import numpy as np
+import pandas as pd
+import xarray as xr
+
 import new_majiq as nm
-import new_majiq.constants as constants
 from new_majiq._run._majiq_args import check_nonnegative_factory
 from new_majiq._run._run import GenericSubcommand
 from new_majiq.logger import get_logger
 
-DESCRIPTION = "Quantify events coverage files"
+DESCRIPTION = "Quantify PSI from PsiCoverage files"
 
 
 def add_args(parser: argparse.ArgumentParser) -> None:
     parser.add_argument(
-        "output",
-        type=Path,
-        help="Path for new-majiq quantification format",
-    )
-    parser.add_argument(
-        "coverage",
+        "psicov",
         type=Path,
         nargs="+",
-        help="Paths to psi coverage files. All must have been generated"
-        " using the same splicegraph",
+        help="Paths to PsiCoverage files. All must have been generated using same splicegraph",
     )
     parser.add_argument(
-        "--pmf-bins",
-        type=check_nonnegative_factory(int, True),
+        "--splicegraph",
+        type=Path,
         default=None,
-        help="If specified, number of discretized probability bins to save"
-        " posterior distribution to (default: %(default)s)",
+        help="If specified, annotate quantifications with splicegraph information",
     )
     parser.add_argument(
         "--quantiles",
         type=float,
         nargs="+",
-        default=[],
-        help="Posterior quantiles to calculate (default: %(default)s)",
-    )
-    parser.add_argument(
-        "--tsv",
-        type=Path,
         default=None,
-        nargs=2,
-        metavar=("splicegraph", "tsv"),
-        help="If specified, save quantifications to TSV annotated by input"
-        " splicegraph",
+        help="If specified, calculate/report PSI posterior quantiles",
     )
     parser.add_argument(
         "--min-experiments",
         type=check_nonnegative_factory(float, True),
-        default=constants.DEFAULT_QUANTIFY_MINEXPERIMENTS,
-        help="Threshold for group filters. If < 1, the fraction of experiments"
-        " in a group that must pass individual filters for a connection to be"
-        " passed. If greater, an absolute number. (default: %(default)s)",
+        default=None,
+        help="If specified, treat samples as replicates and quantify combined coverage for events that passed in at least min experiments (proportion of experiments if < 1)",
     )
-    drop_ex = parser.add_mutually_exclusive_group()
-    drop_ex.add_argument(
-        "--drop-unquantifiable",
-        action="store_true",
-        dest="drop_unquantifiable",
-        default=True,
-        help="Drop records for unquantifiable events, saving storage space"
-        " (default: drop_unquantifiable=%(default)s)",
-    )
-    drop_ex.add_argument(
-        "--keep-unquantifiable",
-        action="store_false",
-        dest="drop_unquantifiable",
-        default=True,
-        help="Keep records for unquantifiable events, ensuring that files"
-        " from different samples but sharing the same events are aligned"
-        " (default: drop_unquantifiable=%(default)s)",
+    parser.add_argument(
+        "--output-tsv",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Path for output TSV file (default: stdout)",
     )
     return
 
 
 def run(args: argparse.Namespace) -> None:
-    if not all(p.exists() for p in args.coverage):
-        missing = sorted(p for p in args.coverage if not p.exists())
+    if not all(p.exists() for p in args.psicov):
+        missing = sorted(p for p in args.psicov if not p.exists())
         raise ValueError(f"Unable to find input coverage ({missing = })")
-    if args.output.exists():
-        raise ValueError(f"Output coverage file {args.output} already exists")
-    if args.tsv:
-        if not args.tsv[0].exists():  # splicegraph
-            raise ValueError(f"Unable to find splicegraph {args.tsv[0]} for TSV output")
-        if args.tsv[1].exists():
-            raise ValueError(f"Output TSV file {args.tsv[1]} already exists")
+    if args.splicegraph and not args.splicegraph.exists():
+        raise ValueError(f"Unable to find input splicegraph {args.splicegraph}")
 
     log = get_logger()
 
-    log.info(f"Opening coverage from {len(args.coverage)} PSI coverage files")
-    psicov = nm.PsiCoverage.from_zarr(args.coverage)
-    log.info(f"Aggregating coverage from {len(psicov.prefixes)} samples")
-    psicov = psicov.sum("aggregate", min_experiments_f=args.min_experiments)
-    if args.drop_unquantifiable:
-        psicov = psicov.drop_unquantifiable()
+    log.info(f"Joining {len(args.psicov)} input PSI coverage files")
+    psicov = nm.PsiCoverage.from_zarr(args.psicov)
+    log.info(f"Using coverage from {psicov.df.sizes['prefix']} input experiments")
+    if args.min_experiments is not None:
+        log.info(f"Aggregating coverage using min-experiments = {args.min_experiments}")
+        psicov = psicov.sum("aggregate", min_experiments_f=args.min_experiments)
 
-    log.info("Quantifying coverage")
-    q = psicov.quantifier_dataset(pmf_bins=args.pmf_bins, quantiles=args.quantiles)
-    q.to_zarr(args.output, mode="w", group=constants.NC_EVENTSQUANTIFIED)
-    psicov.events.to_zarr(args.output, mode="a", group=constants.NC_EVENTS)
+    # initialize list of data frames that will be concatenated (on columns)
+    concat_df: List[pd.DataFrame] = list()
 
-    if args.tsv:
-        log.info("Loading splicegraph for annotated TSV file")
-        sg = nm.SpliceGraph.from_zarr(args.tsv[0])
-        log.info("Saving annotated quantifications to TSV")
-        psicov.as_dataframe(sg).to_csv(args.tsv[1], sep="\t")
+    # did we have a splicegraph to work with? add annotations...
+    if args.splicegraph:
+        log.info(f"Loading splicegraph from {args.splicegraph}")
+        sg = nm.SpliceGraph.from_zarr(args.splicegraph)
+        events = psicov.get_events(sg.introns, sg.junctions)
+        concat_df.append(events.ec_dataframe)
+
+    # get desired quantifications
+    quantify_vars = {
+        "any_passed": psicov.event_passed.any("prefix"),
+        "raw_psi_mean": psicov.raw_posterior_mean,
+        "raw_psi_std": np.sqrt(psicov.raw_posterior_variance),
+        "bootstrap_psi_mean": psicov.bootstrap_posterior_mean,
+        "bootstrap_psi_std": np.sqrt(psicov.bootstrap_posterior_variance),
+        "raw_coverage": psicov.raw_coverage,
+    }
+    if args.quantiles:
+        quantiles = sorted(set(np.round(args.quantiles, 3)))
+        log.info("Will compute the following posterior quantiles: {quantiles}")
+        quantify_vars["approx_psi_quantile"] = psicov.approximate_quantile(quantiles)
+    log.info("Performing quantification")
+    ds_quant = xr.Dataset(quantify_vars).reset_coords(drop=True).load()  # type: ignore[arg-type]
+
+    log.info("Reshaping resulting quantifications to table")
+    # all quantifications but quantiles
+    df_quant = (
+        ds_quant.drop_dims("quantiles", errors="ignore")
+        .drop_vars("any_passed")
+        .to_dataframe()
+        .unstack("prefix")
+        .reorder_levels([1, 0], axis=1)
+        .sort_index(axis=1)
+    )
+    df_quant.columns = [
+        f"{prefix} {var}" if ds_quant.sizes["prefix"] > 1 else var
+        for (prefix, var) in df_quant.columns.values
+    ]
+    concat_df.append(df_quant)
+    if args.quantiles:
+        df_quantiles = (
+            ds_quant["approx_psi_quantile"]
+            .to_series()
+            .unstack(["prefix", "quantiles"])
+            .sort_index(axis=1)
+        )
+        df_quantiles.columns = [
+            f"{prefix} approx_psi_quantile_{q:0.3f}"
+            if ds_quant.sizes["prefix"] > 1
+            else f"approx_psi_quantile_{q:0.3f}"
+            for (prefix, q) in df_quantiles.columns.values
+        ]
+        concat_df.append(df_quantiles)
+    log.info(f"Saving resulting table to {args.output_tsv.name}")
+    # TODO(jaicher): add header of metadata
+    (
+        # concatenate columns together
+        pd.concat(concat_df, axis=1, join="inner")
+        # remove rows where no input passed
+        .loc[ds_quant["any_passed"].values]
+        # numeric columns need at most 4 digits precision
+        .round(4)
+        # save result in TSV format
+        .to_csv(args.output_tsv, sep="\t", index=False)
+    )
     return
 
 
