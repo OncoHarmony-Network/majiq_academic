1a741127303d198517c4e783069c0ec1237a86fb
Joseph K Aicher <jaicher@biociphers.org>
2021-10-12 16:53:28 -0400

    Skip work, avoid warning when using dpsi_discrete on nan data

--- src/gufuncs/beta_mixture/DPsiDiscrete.hpp
+++ src/gufuncs/beta_mixture/DPsiDiscrete.hpp
@@ -88,20 +88,35 @@ static void Outer(
   std::vector<RealT> logp_psi2(n_psibins);
   for (npy_intp i = 0; i < dim_broadcast;
       ++i, ++a1, ++b1, ++a2, ++b2, ++logprior, ++out) {
-    using MajiqInclude::BetaMixture::_LogPMF;
-    // compute log posterior for psi1, psi2
-    _LogPMF(a1.with_stride(inner_stride_a1), b1.with_stride(inner_stride_b1),
-        logp_psi1.begin(), dim_mixture1, n_psibins);
-    _LogPMF(a2.with_stride(inner_stride_a2), b2.with_stride(inner_stride_b2),
-        logp_psi2.begin(), dim_mixture2, n_psibins);
-    // get unnormalized log distribution convolving/interpolating for dpsi
-    // (without dpsi prior)
+    // get buffer with stride for inner loop
+    auto a1_mix = a1.with_stride(inner_stride_a1);
+    auto a2_mix = a2.with_stride(inner_stride_a2);
+    auto b1_mix = b1.with_stride(inner_stride_b1);
+    auto b2_mix = b2.with_stride(inner_stride_b2);
     auto out_bins = out.with_stride(inner_stride_out);
-    using MajiqInclude::DPsiDiscrete::DPsiIndependent;
-    DPsiIndependent(logp_psi1.begin(), logp_psi2.begin(), n_psibins, out_bins);
-    // apply prior
-    using MajiqInclude::DPsiDiscrete::DPsiPosterior;
-    DPsiPosterior(out_bins, logprior.with_stride(inner_stride_prior), dim_bins);
+    // if invalid beta mixtures for psi1 or psi2, make nan
+    using MajiqInclude::BetaMixture::IsInvalid;
+    if (IsInvalid(a1_mix, b1_mix, dim_mixture1)
+        || IsInvalid(a2_mix, b2_mix, dim_mixture2)) {
+      out_bins.fill(dim_bins, std::numeric_limits<RealT>::quiet_NaN());
+    } else {
+      // compute PMFs
+      using MajiqInclude::BetaMixture::_LogPMF;
+      // compute log posterior for psi1, psi2
+      _LogPMF(a1.with_stride(inner_stride_a1), b1.with_stride(inner_stride_b1),
+          logp_psi1.begin(), dim_mixture1, n_psibins);
+      _LogPMF(a2.with_stride(inner_stride_a2), b2.with_stride(inner_stride_b2),
+          logp_psi2.begin(), dim_mixture2, n_psibins);
+      // get unnormalized log distribution convolving/interpolating for dpsi
+      // (without dpsi prior)
+      using MajiqInclude::DPsiDiscrete::DPsiIndependent;
+      DPsiIndependent(
+          logp_psi1.begin(), logp_psi2.begin(), n_psibins, out_bins);
+      // apply prior, renormalize
+      using MajiqInclude::DPsiDiscrete::DPsiPosterior;
+      DPsiPosterior(
+          out_bins, logprior.with_stride(inner_stride_prior), dim_bins);
+    }
   }
   return;
 }
