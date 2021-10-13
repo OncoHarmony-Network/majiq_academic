fa7b20c3324e88957f2b5c28a18f1df45100197b
Joseph K Aicher <jaicher@biociphers.org>
2021-10-13 00:32:51 -0400

    update stubs for beta_mixture

--- src/new_majiq/beta_mixture.pyi
+++ src/new_majiq/beta_mixture.pyi
@@ -2,7 +2,13 @@ import numpy
 
 approximation: numpy.ufunc
 cdf: numpy.ufunc
+dpsi_discrete: numpy.ufunc
 moments: numpy.ufunc
 pdf: numpy.ufunc
 pmf: numpy.ufunc
 quantile: numpy.ufunc
+sample: numpy.ufunc
+ttest_sample: numpy.ufunc
+
+def rng_resize(n: int) -> None: ...
+def rng_seed(seed: int) -> None: ...
