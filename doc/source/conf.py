4ebb7a22177c31dd568cfbb347ed24f8f4e49353
Joseph K Aicher <jaicher@biociphers.org>
2022-01-05 13:07:42 -0500

    LaTeX docs have high itemize depth
    Sphinx can sometimes make deeply nested lists, this updates latex
    preamble to make sure that can compile latex fine in these cases

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -99,3 +99,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
+
+# options for LaTeX output
+latex_elements = {"preamble": r"\usepackage{enumitem}\setlistdepth{99}"}
