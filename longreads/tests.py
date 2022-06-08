2da450ed5929ebba28d8d4e477ca723984a19c2b
pjewell <pjewell@biociphers.org>
2022-06-08 17:50:18 -0400

    -removed module idx column as it didn't seem like we were testing modules with the current test cases -added a few improvements to run test easier while developing.

--- longreads/tests.py
+++ longreads/tests.py
@@ -2,6 +2,13 @@ import csv
 import subprocess
 import os, sys
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Majiq long reads test runner')
+parser.add_argument('--gene-id', type=str, required=False,
+                    help='run test for this specific gene_id instead of all of them')
+args = parser.parse_args()
+
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
@@ -13,12 +20,18 @@ def read_result_file(path):
             yield line
 
 
-subprocess.check_call("python generate_splice_graph.py -o testcases/sg_generated --json testcases/splice_graphs.json --template-sql ../voila/rna_voila/api/model.sql", shell=True)
+generate_str = "python generate_splice_graph.py -o testcases/sg_generated --json testcases/splice_graphs.json --template-sql ../voila/rna_voila/api/model.sql"
+if args.gene_id:
+    generate_str += f' -n {args.gene_id}'
+
+subprocess.check_call(generate_str, shell=True)
 subprocess.check_call("python main.py --majiq-splicegraph-path testcases/sg_generated.sql --flair-gtf-path /home/pjewell/longreads_debug/collapse3.isoforms.gtf --output-path testcases/result -j 1 --debug", shell=True)
 
 errors = False
 
 for expected, actual in zip(read_result_file('testcases/comparison.tsv'), read_result_file('testcases/result/comparison.tsv')):
+    if args.gene_id and not expected['gene_id'] == args.gene_id:
+        continue
     diff = []
     for key in expected.keys():
         if key not in actual:
@@ -31,6 +44,7 @@ for expected, actual in zip(read_result_file('testcases/comparison.tsv'), read_r
         print('~~~~~Error in gene:', expected['gene_id'] )
         for key, _exp, _act in diff:
             print('     -', key, 'expected: ', _exp, 'found: ', _act)
+        break
 
 if not errors:
     print("All is well!")
